caf0baf0da5b9cc8471aeaf7741513409887a44c
gopal gautam <gopalgautam05@gmail.com>
2024-05-23 16:55:33 -0400

    Changed backend createsendtoken

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,16 +9,16 @@ class AuthController {
   createSendToken = async (user, statusCode, req, res) => {
     const token = await this.signToken(user._id);
 
-    const cookieOptions = {
-      expires: new Date(
-        Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-      ),
-      httpOnly: true,
-      secure: req.secure || req.headers["x-forwarded-proto"] === "https",
-      sameSite: "none",
-    };
+    // const cookieOptions = {
+    //   expires: new Date(
+    //     Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    //   ),
+    //   httpOnly: true,
+    //   secure: req.secure || req.headers["x-forwarded-proto"] === "https",
+    //   sameSite: "none",
+    // };
 
-    res.cookie("jwt", token, cookieOptions);
+    // res.cookie("jwt", token, cookieOptions);
 
     //Remove password from output
     user.password = undefined;
@@ -26,6 +26,7 @@ class AuthController {
     res.status(statusCode).json({
       status: "success",
       data: user,
+      token,
     });
   };
 
